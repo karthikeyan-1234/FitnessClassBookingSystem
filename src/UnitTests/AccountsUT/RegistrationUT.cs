376becdc9ff288d7338954c128a8764ab21c0eb1
mail2 <mail2@ARJUNLAPTOP>
2026-04-13 12:20:29 +0530

    UTs for Accounts service [registration flow] added

---
+++ src/UnitTests/AccountsUT/RegistrationUT.cs
@@ -0,0 +1,124 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Application.Services;
+
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Interfaces;
+
+using FluentAssertions;
+
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+using Moq;
+
+using Shared;
+
+namespace AccountsUT
+{
+    public class RegistrationUT
+    {
+
+        private readonly Mock<IUserRepository> _userRepoMock;
+        private readonly Mock<IPasswordHasher> _passwordHasherMock;
+        private readonly Mock<IJwtTokenService> _jwtServiceMock;
+
+        public RegistrationUT()
+        {
+            _userRepoMock = new Mock<IUserRepository>();
+            _passwordHasherMock = new Mock<IPasswordHasher>();
+            _jwtServiceMock = new Mock<IJwtTokenService>();
+
+        }
+
+        void registerMocks(RegisterRequest request)
+        {
+            // Simulate that username does not exist
+            _userRepoMock.Setup(repo => repo.GetByUsernameAsync(request.Username))
+                .ReturnsAsync((User)null!);
+
+            // Simulate hashed password
+            _passwordHasherMock.Setup(hasher => hasher.Hash(request.Password)).Returns("HashedPassprwrd");
+
+            //Simulate JWT generation
+            _jwtServiceMock.Setup(jwt => jwt.GenerateToken(It.IsAny<User>())).Returns("SomeJWTToken");
+
+
+            //Simulate JWT Token expiry
+            _jwtServiceMock.Setup(jwt => jwt.GetTokenExpiry()).Returns(DateTime.UtcNow.AddMinutes(30));
+
+        }
+
+        [Fact]
+        public async Task Register_WithValidData_ReturnsAuthResponse()
+        {
+            // Arrange
+            var request = new RegisterRequest
+            {
+                Username = "john_doe",
+                Password = "Test@123",
+                Role = Role.Member
+            };
+
+            registerMocks(request);
+
+            //Act i.e. Test AuthService's RegisterAsync
+            AuthService _authService = new AuthService(_userRepoMock.Object,
+                                _passwordHasherMock.Object,
+                                _jwtServiceMock.Object);
+
+            var result = await _authService.RegisterAsync(request);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Token.Should().Be("SomeJWTToken");
+            result.Username.Should().Be(request.Username);
+            result.Role.Should().Be("Member");
+
+        }
+
+
+        [Fact]
+        public async Task Register_WithDuplicateUsername_ThrowsDomainException()
+        {
+
+            // Arrange
+            var request = new RegisterRequest
+            {
+                Username = "john_doe",
+                Password = "Test@123",
+                Role = Role.Member
+            };
+
+            var existingUser = new User
+            {
+                Id = System.Guid.NewGuid(),
+                Username = "existing_user",
+                PasswordHash = "someHash",
+                Role = Role.Member
+            };
+
+            registerMocks(request);
+
+            // Override to Simulate that username exists
+            _userRepoMock.Setup(repo => repo.GetByUsernameAsync(request.Username))
+                .ReturnsAsync(existingUser);
+
+            AuthService _authService = new AuthService(_userRepoMock.Object,
+                                _passwordHasherMock.Object,
+                                _jwtServiceMock.Object);
+            try
+            {
+                var result = await _authService.RegisterAsync(request);
+            }
+            catch (Exception ex)
+            {
+                ex.Should().BeOfType<DomainException>();
+            }
+
+
+        }
+
+    }
+
+}
