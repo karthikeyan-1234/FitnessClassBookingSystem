36e220c7bea0fe91e5b8f1fa45a26a62d0ae00f9
mail2 <mail2@ARJUNLAPTOP>
2026-04-27 03:45:13 +0530

    Fixed ILogger issue

--- src/Accounts/Presentation/Controllers/AuthController.cs
+++ src/Accounts/Presentation/Controllers/AuthController.cs
@@ -15,9 +15,9 @@ namespace Presentation.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
-        private readonly ILogger _logger;
+        private readonly ILogger<AuthController> _logger;
 
-        public AuthController(IAuthService authService, ILogger logger)
+        public AuthController(IAuthService authService, ILogger<AuthController> logger)
         {
             _authService = authService;
             _logger = logger;
